8cde1167171bbe621886628d3f2d29dfb490434e
Joney Spark <sparkjoney@gmail.com>
2020-07-12 22:25:56 +0600

    Simplte Calculator JS

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+console.log("Hello Joney");
+
+var number1 = 30;
+var number2 = '40.5';
+
+var number3 = 40;
+number3 = ''+number3;
+
+//number2 = parseInt(number2);
+//number2 = parseFloat(number2);
+number2 = +number2;
+
+var number4 = 0.1;
+var number5 = 0.2;
+
+var total = number4 + number5;
+
+total = total.toFixed(3);
+
+console.log(total);
+
+console.log(number1 + number2);
+
+console.log(typeof number3);
