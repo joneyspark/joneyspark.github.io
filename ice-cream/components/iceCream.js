555523769d6e7c7fb45a96c2234a0d41e113d67e
Joney Spark <sparkjoney@gmail.com>
2021-01-01 21:11:22 +0600

    ice-cream-app-with vue.js

---
+++ ice-cream/components/iceCream.js
@@ -0,0 +1,19 @@
+app.component("ice-cream", {
+	props: {
+		cart: {
+			type: Array,
+			required: true
+		}
+	},
+	template: `<div>
+            <div class="icecream" id="icecream">
+            <p class="cone"></p>
+            <div
+                class="scoop" v-for="cartItem in cart"
+                :class="cartItem.name.toLowerCase()"
+            ></div>
+            <div class="cherry" v-if="cart.length > 0"></div>
+            <div class="question" v-else></div>
+            </div>
+          </div>`
+})
